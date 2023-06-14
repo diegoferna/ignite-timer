5891eae805f2909926497896c1ec9573ea50ef5c
diegoferna <diego.ferna@hotmail.com>
2023-06-14 11:41:36 -0300

    Finalizando tela home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para sua tarefa"
+            list="task-sugestions"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 4"></option>
+            <option value="Banana"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">Vou trabalhar em</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
