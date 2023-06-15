0a4193c0617caff657faa7a265427be90bcc5569
diegoferna <diego.ferna@hotmail.com>
2023-06-15 17:09:57 -0300

    finalizando page history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,59 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statuscolor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statuscolor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statuscolor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statuscolor="red">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
